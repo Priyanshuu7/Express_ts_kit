f2d988e4f28beee27ee6a49c752fa21d8a724ee5
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-06-19 15:20:34 +0530

    Updated kit

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response) => {
-  return res.send("It's working 🙌");
+  return res.send("Server is Working");
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
